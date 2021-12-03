b0daffc60b61573cbda6a90ffa678f748c533b91
Marcos Strapazon <marcos_strapazon@sicredi.com.br>
2021-12-02 22:12:04 -0300

    Initial Commit

---
+++ src/main/java/com/marcososft/rhdbexplorer/RhdbExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.marcososft.rhdbexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhdbExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhdbExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcososft/rhdbexplorer/RhdbExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcososft.rhdbexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhdbExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
